55c7e51979f49919e18f43a42f2f1ce9def9a49e
Teran45 <89353523+Teran45@users.noreply.github.com>
2022-05-24 12:07:14 +0300

    Add files via upload

---
+++ ClickHouse/_show_create_default_calculated_data__202205160849.sql
@@ -0,0 +1,40 @@
+INSERT INTO `show create default.calculated_data` (`statement`) VALUES
+	 ('CREATE VIEW default.calculated_data
+(
+    `data` DateTime,
+    `user_id` String,
+    `session_id` UInt32,
+    `session_duration` Int32
+) AS
+WITH a AS
+    (
+        SELECT
+            event_time AS data,
+            user_id,
+            event_id,
+            multiIf((event_time - neighbor(event_time, -1)) > 900, ''start'', (neighbor(event_time, 1) - event_time) > 900, ''stop'', NULL) AS act_start
+        FROM
+        (
+            SELECT *
+            FROM default.raw_data
+            ORDER BY
+                user_id ASC,
+                event_id ASC,
+                event_time ASC
+        )
+        WHERE (act_start = ''start'') OR (act_start = ''stop'')
+    )
+SELECT
+    data,
+    user_id,
+    rand(1) AS session_id,
+    stat_data - data AS session_duration
+FROM
+(
+    SELECT
+        *,
+        neighbor(act_start, 1) AS stat,
+        neighbor(data, 1) AS stat_data
+    FROM a
+)
+WHERE (stat = ''stop'') AND (session_duration > 0)');
---
+++ ClickHouse/_show_create_default_raw_data__202205160850.sql
@@ -0,0 +1,10 @@
+INSERT INTO `show create default.raw_data` (`statement`) VALUES
+	 ('CREATE TABLE default.raw_data
+(
+    `user_id` String,
+    `event_time` DateTime,
+    `event_id` String
+)
+ENGINE = MergeTree
+ORDER BY user_id
+SETTINGS index_granularity = 8192');
